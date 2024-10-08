cfdbac24ffccbb48786d7de452a98d7ecf132756
Mohammed Hassan Ahmed <123543218+MohammedHasanAhmed@users.noreply.github.com>
2024-10-08 11:57:30 +0300

    Add padding in body of style css and add View start

--- Inventory-Management-System/obj/Debug/net8.0/Inventory-Management-System.AssemblyInfo.cs
+++ Inventory-Management-System/obj/Debug/net8.0/Inventory-Management-System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e410191183ef8cdc52e3a4e6843dcbaa104bc671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d3729d89fd6ce7aa98a7da6a45c23f3b6c30e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
