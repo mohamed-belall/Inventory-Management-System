96685c37c0d298c13be5c248f32d27495982cf22
Mohammed Hassan Ahmed <123543218+MohammedHasanAhmed@users.noreply.github.com>
2024-10-15 00:02:29 +0300

    add authorization and authontication

--- Inventory-Management-System/Controllers/Authontication/AccountController.cs
+++ Inventory-Management-System/Controllers/Authontication/AccountController.cs
@@ -62,9 +62,9 @@ namespace Inventory_Management_System.Controllers.Authontication
                 IdentityResult result = await userManager.CreateAsync(appUser, registerViewModel.Password);
                 if (result.Succeeded)
                 {
-                    // assign to role
+
                     await userManager.AddToRoleAsync(appUser, registerViewModel.Role);
-                    
+
                     // Create Cookie
                     await signInManager.SignInAsync(appUser, false);
                     return RedirectToAction("Login", "Account");
@@ -107,6 +107,77 @@ namespace Inventory_Management_System.Controllers.Authontication
             return View("Login", loginUserViewModel);
         }
 
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> VerifyEmail(VerifyEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(model.Email);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Something is wrong!");
+                    return View(model);
+                }
+                else
+                {
+                    return RedirectToAction("ChangePassword", "Account", new { username = user.UserName });
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult ChangePassword(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("VerifyEmail", "Account");
+            }
+            return View(new ChangePasswordViewModel { Email = username });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(model.Email);
+                if (user != null)
+                {
+                    var result = await userManager.RemovePasswordAsync(user);
+                    if (result.Succeeded)
+                    {
+                        result = await userManager.AddPasswordAsync(user, model.NewPassword);
+                        return RedirectToAction("Login", "Account");
+                    }
+                    else
+                    {
+
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+
+                        return View(model);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email not found!");
+                    return View(model);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Something went wrong. try again.");
+                return View(model);
+            }
+        }
 
         public async Task<IActionResult> SignOut()
         {
---
+++ Inventory-Management-System/ViewModel/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.ViewModel
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [Display(Name = "User Name")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match.")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
